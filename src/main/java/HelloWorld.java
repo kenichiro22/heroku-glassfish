04d917559ddb711f5ff59782ad8ffc93c3fdcb21
unknown <ktana@.(none)>
2011-10-27 23:19:55 +0900

    init

---
+++ src/main/java/HelloWorld.java
@@ -0,0 +1,35 @@
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.servlet.ServletContextHandler;
+import org.eclipse.jetty.servlet.ServletHolder;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: ktana
+ * Date: 11/10/26
+ * Time: 23:37
+ * To change this template use File | Settings | File Templates.
+ */
+public class HelloWorld extends HttpServlet{
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+            throws ServletException, IOException {
+        resp.getWriter().print("Hello from Java!\n");
+    }
+
+    public static void main(String[] args) throws Exception{
+        Server server = new Server(Integer.valueOf(System.getenv("PORT")));
+        ServletContextHandler context = new ServletContextHandler(ServletContextHandler.SESSIONS);
+        context.setContextPath("/");
+        server.setHandler(context);
+        context.addServlet(new ServletHolder(new HelloWorld()),"/*");
+        server.start();
+        server.join();
+    }
+
+}
